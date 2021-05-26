b5cb505ddee77472a7488803ba162b237f26e7f5
defrieantoe1999 <79563472+defrieantoe1999@users.noreply.github.com>
2021-05-26 08:37:09 +0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -4,9 +4,10 @@ class _BotCommands:
         self.MirrorCommand = 'koco'
         self.UnzipMirrorCommand = 'unzipkoco'
         self.TarMirrorCommand = 'tarkoco'
-        self.CancelMirror = 'cancel'
-        self.CancelAllCommand = 'cancelall'
+        self.CancelMirror = 'batal'
+        self.CancelAllCommand = 'gagal'
         self.ListCommand = 'list'
+        self.SpeedCommand = 'speedtest'
         self.StatusCommand = 'status'
         self.AuthorizeCommand = 'authorize'
         self.UnAuthorizeCommand = 'unauthorize'
@@ -15,11 +16,9 @@ class _BotCommands:
         self.StatsCommand = 'stats'
         self.HelpCommand = 'tolong'
         self.LogCommand = 'log'
-        self.SpeedCommand = 'gass'
-        self.CloneCommand = "kelon"
+        self.CloneCommand = "kopi"
         self.WatchCommand = 'nonton'
-        self.TarWatchCommand = 'tartonton'
-        self.DeleteCommand = 'hapus'
-        self.UsageCommand = 'usage'
+        self.TarWatchCommand = 'tarwatch'
+        self.deleteCommand = 'hapus'
 
 BotCommands = _BotCommands()
